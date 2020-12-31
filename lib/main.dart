d6d65c511aab2a54dcb7a261561163071d1a8873
PrestoSole <yalather70@pm.me>
2020-12-31 14:23:11 +0300

    Fixed weird import caused by auto import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-import 'file:///C:/Users/PrestoSole/StudioProjects/polaris/lib/Pages/home_page.dart';
+import 'package:polaris/Pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
