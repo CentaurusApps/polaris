c28e7a53e005c3b453013088cc9fec078ba701fa
PrestoSole <yalather70@pm.me>
2021-01-02 11:55:06 +0300

    Theme refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,8 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       theme: ThemeData.light().copyWith(
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: Theme.of(context).accentColor,
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Color(0xFFF40057),
         ),
         accentColor: const Color(0xFFF40057),
         scaffoldBackgroundColor: Colors.white,
