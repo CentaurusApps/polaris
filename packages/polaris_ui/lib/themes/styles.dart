99d78e177928dfa8d3eba3eb4209b72c6664d8e6
PrestoSole <47897195+PrestoSole@users.noreply.github.com>
2021-01-18 13:44:38 +0300

    Move apptheme to styles (#22)
    * Move SortType to domain and create SortOrder
    
    * Move AppTheme to styles.dart
    
    Co-authored-by: mhmdanas <32234660+mhmdanas@users.noreply.github.com>

---
+++ packages/polaris_ui/lib/themes/styles.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AppTheme {
+  static final ThemeData black = ThemeData.dark().copyWith(
+    textTheme: const TextTheme(subtitle1: TextStyle(color: Colors.white)),
+    snackBarTheme: const SnackBarThemeData(backgroundColor: Color(0xFFE6E6E6)),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Color(0xFF80D8FE),
+    ),
+    popupMenuTheme: const PopupMenuThemeData(color: Color(0xFF202125)),
+    dialogBackgroundColor: const Color(0xFF202125),
+    toggleableActiveColor: const Color(0xFF80D8FE),
+    accentColor: const Color(0xFF80D8FE),
+    primaryColor: Colors.black,
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: const AppBarTheme(
+      actionsIconTheme: IconThemeData(color: Colors.white),
+      iconTheme: IconThemeData(color: Colors.white),
+      textTheme: TextTheme(subtitle1: TextStyle(color: Colors.white)),
+      color: Colors.black,
+      brightness: Brightness.dark,
+    ),
+  );
+
+  final ThemeData dark = ThemeData();
+
+  static final ThemeData light = ThemeData.light().copyWith(
+    textTheme: TextTheme(subtitle1: TextStyle(color: Colors.grey.shade900)),
+    snackBarTheme: const SnackBarThemeData(backgroundColor: Color(0xFF6F6F6F)),
+    toggleableActiveColor: ThemeData.light().accentColor,
+    scaffoldBackgroundColor: const Color(0xFFF2F2F2),
+    appBarTheme: AppBarTheme(
+      textTheme: TextTheme(subtitle1: TextStyle(color: Colors.grey.shade900)),
+      color: const Color(0xFFF2F2F2),
+      actionsIconTheme: IconThemeData(color: Colors.grey.shade900),
+      iconTheme: IconThemeData(color: Colors.grey.shade900),
+      brightness: Brightness.light,
+    ),
+  );
+
+  static void blackBarsColor() {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.black,
+      systemNavigationBarIconBrightness: Brightness.dark,
+      systemNavigationBarDividerColor: Colors.black,
+      statusBarColor: Colors.black,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.dark,
+    ));
+  }
+
+  static void lightBarsColor() {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      systemNavigationBarColor: Color(0xFFF2F2F2),
+      systemNavigationBarIconBrightness: Brightness.light,
+      systemNavigationBarDividerColor: Color(0xFFF2F2F2),
+      statusBarColor: Color(0xFFF2F2F2),
+      statusBarBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.light,
+    ));
+  }
+}
